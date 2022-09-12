LineageOS/android_device_motorola_berlin

berlin: Update to S1RGS32.53-18-22-2
Change-Id: I49567bea26fd5240daca6d3b3b7868e95f44ae40

lineage_berlin.mk
@@ -34,6 +34,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=berlin_retail \
-    PRIVATE_BUILD_DESC="berlin_global-user 11 S1RG32.53-18-11-7 7be15 release-keys"
+    PRIVATE_BUILD_DESC="berlin_global-user 12 S1RG32.53-18-22-2 0e3b7 release-keys"
 
-BUILD_FINGERPRINT := motorola/berlin_global/berlin:11/S1RGS32.53-18-11-7/7be15:user/release-keys
+BUILD_FINGERPRINT := motorola/berlin_global/berlin:12/S1RGS32M.53-18-22-2/0e3b7:user/release-keys

NaassomDourado/android_device_motorola_berlin_old

lineage_berlin.mk
@@ -57,6 +57,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=berlin_global \
     PRODUCT_NAME="berlin" \
-    PRIVATE_BUILD_DESC="berlin_global-user 12 S1RG32.53-18-11-7 7be15 release-keys"
+    PRIVATE_BUILD_DESC="berlin_global-user 12 S1RG32.53-18-22-2 0e3b7 release-keys"
 
-BUILD_FINGERPRINT := motorola/berlin_global/berlin:12/S1RGS32.53-18-11-7/7be15:user/release-keys
+BUILD_FINGERPRINT := motorola/berlin_global/berlin:12/S1RGS32.53-18-22-2/0e3b7:user/release-keys